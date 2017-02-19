6567fb9a58debc49cd97ab9bcfe9fbfa0bf1f398
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2017-02-19 15:00:11 +0100

    added container for firefox syncserver

---
+++ syncserver/README.md
@@ -0,0 +1,6 @@
+Firefox SyncServer
+==================
+
+![http://www.pihomeserver.fr/wp-content/uploads/2013/06/sync-logo.png](firefox syncserver)
+
+[https://github.com/mozilla-services/syncserver](firefox syncserver repo)
---
+++ syncserver/run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cd /sync/server
+
+SECRET=$(cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 50 | head -n1)
+
+echo "\
+[server:main]
+use = egg:gunicorn
+host = 0.0.0.0
+port = 5000
+workers = 1
+timeout = 30
+errorlog = -
+loglevel = warning
+forwarded_allow_ips = *
+
+[app:main]
+use = egg:syncserver
+
+[syncserver]
+public_url = ${URL}
+secret = ${SECRET}
+force_wsgi_environ = false
+" > /sync/server/syncserver.ini
+
+chown -R ${UID}:${GID} .
+
+su-exec ${UID}:${GID} make serve
