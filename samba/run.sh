342fb08194435bf118d17238d77e4d6c0fe0c004
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2017-07-20 23:47:36 +0200

    Refresh samba image
    - Switching to alpine 3.6
    - Adding su-exec and s6 for better process control

--- samba/run.sh
+++ samba/run.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-su samba 
-nmbd -Ds /config/smb.conf 
-# < /dev/null is necessary because smbd (and nmbd too) stops if launched in foreground and receiving EOF from stdin.
-smbd -FSs /config/smb.conf < /dev/null
+
+su-exec samba:samba /bin/s6-svscan /etc/s6.d
