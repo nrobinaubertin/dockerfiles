a81547b2ea5c84204fb7aaee52726ca4c54047ea
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2017-10-02 08:48:09 +0200

    Allow persistent db over volume in rftg image

--- NOT_READY/rftg/run.sh
+++ NOT_READY/rftg/run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# cd /rftg
+# rm -rf /var/lib/mysql && mkdir -p /var/lib/mysql /var/run/mysqld
+# chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+# ensure that /var/run/mysqld (used for socket and lock files) is writable regardless of the UID our mysqld instance ends up having at runtime
+# chmod 777 /var/run/mysqld
+
+#chown root:root -R /var/lib/mysql
+# /usr/bin/mysql_install_db --user=mysql
 /etc/init.d/mysql start
-exec ./rftgserver
+# mysqld
+# mysql < /rftg/server-schema.sql
+# /etc/init.d/mysql stop
+exec /rftg/rftgserver
