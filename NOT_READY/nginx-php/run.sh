c94bc0b24a95adeeb254bb28d2938a7c7cfb4b12
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-08-02 15:37:34 +0200

    Rework nginx-php container

--- NOT_READY/nginx-php/run.sh
+++ NOT_READY/nginx-php/run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-mv -n /nginx/nginx.conf /etc/nginx/nginx.conf
-chown -R "${UID}:${GID}" /php /nginx /etc/s6.d /var/log/nginx /var/tmp/nginx
+
+mkdir -p /run/nginx
+chown -R "${UID}:${GID}" \
+    /etc/s6.d \
+    /var/log/nginx \
+    /var/tmp/nginx \
+    /etc/nginx \
+    /run/nginx \
+    /var/log/php7 \
+    /etc/php7
+
 su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
