aedaf12b46d62a378134932f12bb0b39a3513146
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-08-04 13:55:54 +0000

    Make cron job work for the acme container

--- NOT_READY/acme/run.sh
+++ NOT_READY/acme/run.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-openssl dhparam -out /certs/dhparams.pem 2048
-chmod 755 -R /http
-chown "${UID}:${GID}" -R /http /etc/s6.d /certs /acme
+# create cron task
+crontab -r
+echo "* * * * * /usr/local/bin/renew-certificates" | crontab -
+
+if ! [ -f /certs/dhparams.pem ]; then
+    openssl dhparam -out /certs/dhparams.pem 2048
+fi
+chmod 755 -R /http /var/log
+chown "${UID}:${GID}" -R /http /etc/s6.d /certs /acme /var/log
 su-exec "${UID}:${GID}" thttpd -d /http -p 8080
 echo "Waiting a bit for thttpd..."
 sleep 1
 if pgrep thttpd > /dev/null; then
-    domains=$(echo ";"${DOMAINS} | tr -d '\n' | sed 's/;\+$//' | sed 's/;\+/ -d /g')
+    domains="$(echo ";"${DOMAINS} | tr -d '\n' | sed 's/;\+$//' | sed 's/;\+/ -d /g')"
     su-exec "${UID}:${GID}" acme.sh --issue ${domains} -w /http/ --certhome "${CERTHOME}" --home "${HOME}" --debug
     killall thttpd
-    su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
+    exec /bin/s6-svscan /etc/s6.d
 else
     echo "thttpd did not start. Aborting."
 fi
