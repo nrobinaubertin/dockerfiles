6a8a7d5f11ace7f101445913213e5209f87886eb
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-09-02 19:48:32 +0200

    Execute traefik as root

--- NOT_READY/traefik/run.sh
+++ NOT_READY/traefik/run.sh
@@ -7,5 +7,4 @@ crontab -r
 # every 5mn
 echo "*/5 * * * * /usr/local/bin/dumpcerts.sh /data/acme.json /data/certs" | crontab -
 
-chown "${UID}:${GID}" -R /data /config /etc/s6.d
 exec /bin/s6-svscan /etc/s6.d
