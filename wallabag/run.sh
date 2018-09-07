4cc799f65301dd3ae601f8e972819c863c6ceb05
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-09-07 19:04:03 +0200

    Fix wallabag

--- wallabag/run.sh
+++ wallabag/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if ! [ -d /wallabag/data/db ]
-then
+if ! [ -d /wallabag/data/db ]; then
     mv /wallabag/db /wallabag/data/db
 fi
+sed -i -E "s|https://0.0.0.0|${DOMAIN}|g" /wallabag/app/config/parameters.yml
 php /wallabag/bin/console cache:clear --env=prod
-chown -R "${UID}:${GID}" /wallabag/web /wallabag/app /wallabag/src /wallabag/data /wallabag/var /php /nginx /etc/s6.d /var/log/nginx /var/tmp/nginx
+chown -R "${UID}:${GID}" /wallabag /etc/php7 /etc/nginx /etc/s6.d /var/log/ /var/tmp/ /run/nginx
 su-exec "$UID:$GID" /bin/s6-svscan /etc/s6.d
