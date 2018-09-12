0c04a0b3912fd3eaef5064722bce6250467972af
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-09-12 16:49:24 +0200

    Try to fix prosody container

--- NOT_READY/prosody/prosody.cfg.lua
+++ NOT_READY/prosody/prosody.cfg.lua
@@ -9,9 +9,12 @@ consider_bosh_secure = true
 http_paths = {
     bosh = "/http-bind"; -- Serve BOSH at /http-bind
     files = "/"; -- Serve files from the base URL
+    -- conversejs = "/";
 }
 http_index_files = { "index.html", "index.htm" }
 http_files_dir = "/www";
+-- http_dir_listing = true;
+-- http_interfaces = { "*" }
 http_upload_path = "/data";
 http_upload_file_size_limit = 1024 * 1024 * 10; -- 10Mo
 http_upload_expire_after = 60 * 60 * 24 * 14; -- 2 weeks in seconds
@@ -23,7 +26,31 @@ c2s_require_encryption = true
 authentication = "internal_hashed"
 storage = "internal"
 
-plugin_paths = { "/prosody_modules" }
+conversejs_options = {
+    view_mode = "fullscreen";
+    auto_away = 300;
+    auto_reconnect = false;
+    message_archiving = "always";
+    default_domain = "<DOMAIN>";
+}
+
+admins = { "<ADMIN>" }
+
+ssl = {
+    certificate = "/etc/prosody/certs/<DOMAIN>.crt";
+    key = "/etc/prosody/certs/<DOMAIN>.key";
+    protocol = "tlsv1+"
+}
+
+Component "<SUBDOMAIN>.<DOMAIN>" "muc"
+    restrict_room_creation = false;
+    max_history_messages = 50;
+
+VirtualHost "<DOMAIN>"
+    enabled = true
+    http_host = "<SUBDOMAIN>.<DOMAIN>"
+
+-- plugin_paths = { "/prosody_modules" }
 modules_enabled = {
 
     -- OMEMO support (https://serverfault.com/questions/835635/what-prosody-modules-do-i-need-to-support-conversations)
@@ -56,13 +83,13 @@ modules_enabled = {
     "time"; -- Let others know the time here on this server
     "ping"; -- Replies to XMPP pings with pongs
     "pep"; -- Enables users to publish their mood, activity, playing music and more
-    "register"; -- Allow users to register on this server using a client and change passwords
+    -- "register"; -- Allow users to register on this server using a client and change passwords
 
     -- HTTP modules
     "http"; -- https://prosody.im/doc/http
     "http_upload"; -- https://modules.prosody.im/mod_http_upload.html
     "bosh"; -- Enable BOSH clients, aka "Jabber over HTTP"
-    "http_files"; -- Serve static files from a directory over HTTP
+    -- "http_files"; -- Serve static files from a directory over HTTP
 
     -- Other specific functionality
     -- "groups"; -- Shared roster support
@@ -70,6 +97,7 @@ modules_enabled = {
     -- "welcome"; -- Welcome users who register accounts
     "watchregistrations"; -- Alert admins of registrations
     -- "motd"; -- Send a message to users when they log in
+    "conversejs"; -- web based client
 }
 
 -- These modules are auto-loaded, but should you want
--- NOT_READY/prosody/run.sh
+++ NOT_READY/prosody/run.sh
@@ -1,26 +1,8 @@
 #!/bin/sh
 
-sed -i -e "s/DOMAIN/${SUBDOMAIN}.${DOMAIN}/g" /www/index.html
-
-echo '
-
-admins = { "'"${ADMIN}"'" }
-
-ssl = {
-    certificate = "/etc/prosody/certs/'"${DOMAIN}"'.crt";
-    key = "/etc/prosody/certs/'"${DOMAIN}"'.key";
-    protocol = "tlsv1+"
-}
-
-Component "${SUBDOMAIN}.'"${DOMAIN}"'" "muc"
-    restrict_room_creation = false;
-    max_history_messages = 50;
-
-VirtualHost "'"${DOMAIN}"'"
-	enabled = true
-    http_host = "${SUBDOMAIN}.'"${DOMAIN}"'"
-
-' >> /etc/prosody/prosody.cfg.lua
+sed -i -e "s|<DOMAIN>|${DOMAIN}|g" \
+       -e "s|<SUBDOMAIN>|${SUBDOMAIN}|g" \
+       -e "s|<ADMIN>|${ADMIN}|g" /etc/prosody/prosody.cfg.lua
 
 openssl req -new -x509 -days 365 -nodes \
     -out "/etc/prosody/certs/${DOMAIN}.crt" -newkey rsa:2048 \
@@ -30,7 +12,8 @@ openssl req -new -x509 -days 365 -nodes \
     -out "/etc/prosody/certs/${SUBDOMAIN}.${DOMAIN}.crt" -newkey rsa:2048 \
     -keyout "/etc/prosody/certs/${SUBDOMAIN}.${DOMAIN}.key" -subj "/CN=${SUBDOMAIN}.${DOMAIN}"
 
-chown -R "${UID}:${GID}" /etc/s6.d /data /var/lib/prosody /www /etc/prosody
-chmod 755 -R /data /var/lib/prosody /www /etc/prosody
+mkdir -p /var/run/prosody
+chown -R "${UID}:${GID}" /etc/s6.d /data /var/lib/prosody /etc/prosody /var/run/prosody /usr/lib/prosody/modules
+chmod 755 -R /data /var/lib/prosody /etc/prosody
 
 su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
