3d71639edaad019c8818dd1b47eb3b3d891e4f11
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-09-20 10:56:59 +0200

    Update wordpress container

--- NOT_READY/wordpress/run.sh
+++ NOT_READY/wordpress/run.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
+set -xe
+
 mv -n /wordpress/wp-content-original/* /wordpress/wp-content/
-mkdir -p /var/lib/nginx/logs /nginx/logs /nginx/tmp
 
-AUTH_KEY=$(cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 50 | head -n1)
-sed -i -e "s|<AUTH_KEY>|${AUTH_KEY}|g" /wordpress/wp-config.php
-SECURE_AUTH_KEY=$(cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 50 | head -n1)
-sed -i -e "s|<SECURE_AUTH_KEY>|${SECURE_AUTH_KEY}|g" /wordpress/wp-config.php
-LOGGED_IN_KEY=$(cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 50 | head -n1)
-sed -i -e "s|<LOGGED_IN_KEY>|${LOGGED_IN_KEY}|g" /wordpress/wp-config.php
-NONCE_KEY=$(cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 50 | head -n1)
-sed -i -e "s|<NONCE_KEY>|${NONCE_KEY}|g" /wordpress/wp-config.php
-AUTH_SALT=$(cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 50 | head -n1)
-sed -i -e "s|<AUTH_SALT>|${AUTH_SALT}|g" /wordpress/wp-config.php
-SECURE_AUTH_SALT=$(cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 50 | head -n1)
-sed -i -e "s|<SECURE_AUTH_SALT>|${SECURE_AUTH_SALT}|g" /wordpress/wp-config.php
-LOGGED_IN_SALT=$(cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 50 | head -n1)
-sed -i -e "s|<LOGGED_IN_SALT>|${LOGGED_IN_SALT}|g" /wordpress/wp-config.php
-NONCE_SALT=$(cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 50 | head -n1)
-sed -i -e "s|<NONCE_SALT>|${NONCE_SALT}|g" /wordpress/wp-config.php
+sed -i -e "s|<AUTH_KEY>|$(openssl rand -base64 48)|g" \
+       -e "s|<SECURE_AUTH_KEY>|$(openssl rand -base64 48)|g" \
+       -e "s|<LOGGED_IN_KEY>|$(openssl rand -base64 48)|g" \
+       -e "s|<NONCE_KEY>|$(openssl rand -base64 48)|g" \
+       -e "s|<AUTH_SALT>|$(openssl rand -base64 48)|g" \
+       -e "s|<SECURE_AUTH_SALT>|$(openssl rand -base64 48)|g" \
+       -e "s|<LOGGED_IN_SALT>|$(openssl rand -base64 48)|g" \
+       -e "s|<NONCE_SALT>|$(openssl rand -base64 48)|g" /wordpress/wp-config.php
 
-chown -R "${UID}:${GID}" /php /nginx /etc/s6.d /run /wordpress /var/lib/nginx /var/tmp/nginx /var/log/nginx
-su-exec "${UID}:${GID}" run2.sh
+mkdir -p /run/nginx
+chown -R "${UID}:${GID}" /etc/s6.d /wordpress /etc/php7 /etc/nginx /var/lib/nginx /var/log /run /var/tmp/nginx
+su-exec "${UID}:${GID}" mysql_install_db --datadir="/wordpress/wp-content/mysql"
+(
+    sleep 10
+    su-exec "${UID}:${GID}" mysqladmin -u root password "root"
+    su-exec "${UID}:${GID}" mysqladmin -u root --password="root" create "wordpress"
+) &
+exec su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
