39ee1be345a49f4ee231501e11aac4607212cb0e
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-09-20 11:04:16 +0200

    Fix

--- NOT_READY/wordpress/run2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mysql_install_db --datadir="/wordpress/wp-content/mysql"
-(
-    sleep 10
-    mysqladmin -u root password "root"
-    mysqladmin -u root --password="root" create "wordpress"
-) &
-exec /bin/s6-svscan /etc/s6.d
