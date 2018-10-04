4d957623c3611a2bbfa9e269e74876c376073765
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-10-04 22:15:48 +0200

    Fix syncthing container

--- NOT_READY/syncthing/run.sh
+++ NOT_READY/syncthing/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /data/syncthing
-chown -R "${UID}:${GID}" /syncthing /etc/s6.d /usr/local/bin
+chown -R "${UID}:${GID}" /etc/s6.d /usr/local/bin /data
 exec su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
