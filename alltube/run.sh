23d4389d1f4705fda4f3baff2db8b410f78d5d5e
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-10-04 18:49:44 +0200

    Fix alltube container

--- alltube/run.sh
+++ alltube/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /run/nginx
 chown -R "alltube:alltube" /etc/s6.d /var/tmp/nginx /alltube /etc/php7 /etc/nginx /var/log /run
-su-exec "alltube:alltube" /bin/s6-svscan /etc/s6.d
+exec su-exec "alltube:alltube" /bin/s6-svscan /etc/s6.d
