9402d9a3817ae66daca32e1476598533fc76b573
Niels Robin-Aubertin <git@niels.fr>
2018-12-09 11:16:16 +0100

    Add certificates to data dir

--- NOT_READY/prosody/run.sh
+++ NOT_READY/prosody/run.sh
@@ -6,11 +6,12 @@ sed -i -e "s|<DOMAIN>|${DOMAIN}|g" \
        -e "s|<HTTP_UPLOAD_QUOTA>|${HTTP_UPLOAD_QUOTA}|g" \
        -e "s|<ADMIN>|${ADMIN}|g" /etc/prosody/prosody.cfg.lua
 
-openssl req -new -x509 -days 3650 -nodes \
-    -out "/etc/prosody/certs/cert.pem" -newkey rsa:2048 \
-    -keyout "/etc/prosody/certs/key.pem" -subj "/CN=${DOMAIN}"
+mkdir -p /var/run/prosody /data/certs
 
-mkdir -p /var/run/prosody
+openssl req -new -x509 -days 3650 -nodes \
+    -out "/data/certs/cert.pem" -newkey rsa:4096 \
+    -keyout "/data/certs/key.pem" -subj "/CN=${DOMAIN}"
+
 chown -R "${UID}:${GID}" /etc/s6.d /data /var/lib/prosody /etc/prosody /var/run/prosody /usr/lib/prosody/modules
 chmod 755 -R /data /var/lib/prosody /etc/prosody
 
