e99d0601a9d28f939325820dea198b1a582c6f28
Niels Robin-Aubertin <git@niels.fr>
2019-04-25 14:58:55 +0200

    Always use the latest version of youtube-dl

--- alltube/run.sh
+++ alltube/run.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-mkdir -p /run/nginx
+
+set -x
+
+# create cron task
+crontab -r
+# everyday at 1:22 AM
+echo "22 1 * * * update_ytdl.sh" | crontab -
+
+mkdir -p /run/nginx /usr/local/bin/youtube-dl
+/usr/local/bin/update_ytdl.sh
+
 chown -R "alltube:alltube" /etc/s6.d /var/tmp/nginx /alltube /etc/php7 /etc/nginx /var/log /run
 exec su-exec "alltube:alltube" /bin/s6-svscan /etc/s6.d
