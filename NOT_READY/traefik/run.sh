ef5457a8f3df0f10b089829874fa7920e5bf7f8f
Niels Robin-Aubertin <git@niels.fr>
2019-06-13 08:57:46 +0200

    Add logrotate to traefik

--- NOT_READY/traefik/run.sh
+++ NOT_READY/traefik/run.sh
@@ -4,9 +4,11 @@ sed -i -e "s|<EMAIL>|${EMAIL}|g" /config/traefik.toml
 
 # create cron task
 crontab -r
-# every hour
-echo "0 * * * * /usr/local/bin/dumpcerts.sh /data/acme.json /data/certs" | crontab -
+echo "\
+0 * * * * /usr/local/bin/dumpcerts.sh /data/acme.json /data/certs\
+0 * * * * logrotate /config/logrotate.conf\
+" | crontab -
 
-mkdir -p /data/certs
+mkdir -p /data/certs /data/logs
 
 exec /bin/s6-svscan /etc/s6.d
