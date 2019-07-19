64b42e52233452f7bbec2528a31c0d29e2804d0f
Niels Robin-Aubertin <git@niels.fr>
2019-07-19 15:37:42 +0200

    Add freshrss container

---
+++ NOT_READY/freshrss/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -xe
+mkdir -p /run/nginx
+chown -R "${UID}:${GID}" /freshrss /etc/php7 /etc/nginx /etc/s6.d /var/log/ /var/tmp/ /run/nginx /var/run/
+su-exec "$UID:$GID" /bin/s6-svscan /etc/s6.d
