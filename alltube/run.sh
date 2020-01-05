f38c9a674229a6db93483519566980acdcbbd1f8
Niels Robin-Aubertin <git@niels.fr>
2020-01-05 12:16:15 +0100

    Fix alltube

--- alltube/run.sh
+++ alltube/run.sh
@@ -10,5 +10,5 @@ echo "22 1 * * * update_ytdl.sh" | crontab -
 mkdir -p /run/nginx
 /usr/local/bin/update_ytdl.sh
 
-chown -R "alltube:alltube" /etc/s6.d /var/tmp/nginx /alltube /etc/php7 /etc/nginx /var/log /run
+chown -R "alltube:alltube" /etc/s6.d /var/tmp/nginx /alltube /etc/php7 /etc/nginx /var/log /run /var/lib/nginx
 exec su-exec "alltube:alltube" /bin/s6-svscan /etc/s6.d
