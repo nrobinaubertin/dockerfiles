e31373a7feb81bbd453e72f55f837431b698d21f
Niels Robin-Aubertin <git@niels.fr>
2020-01-06 17:17:39 +0100

    Fix nginx issue in freshrss

--- NOT_READY/freshrss/run.sh
+++ NOT_READY/freshrss/run.sh
@@ -4,5 +4,5 @@ if [ -z "$(ls -A /freshrss/data)" ]; then
     cp -r /freshrss/data_default/* /freshrss/data
 fi
 mkdir -p /run/nginx
-chown -R "${UID}:${GID}" /freshrss /etc/php7 /etc/nginx /etc/s6.d /var/log/ /var/tmp/ /run/nginx /var/run/
+chown -R "${UID}:${GID}" /freshrss /etc/php7 /etc/nginx /etc/s6.d /var/log/ /var/tmp/ /run/nginx /var/run/ /var/lib/nginx
 su-exec "$UID:$GID" /bin/s6-svscan /etc/s6.d
