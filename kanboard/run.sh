7373c719c192fe664e5a4c65188a9b4e77eafcae
Niels Robin-Aubertin <git@niels.fr>
2020-01-06 17:07:17 +0100

    Fix nginx issue with kanboard

--- kanboard/run.sh
+++ kanboard/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -xe
 mkdir -p /run/nginx
-chown -R "${UID}:${GID}" /kanboard /etc/php7 /etc/nginx /etc/s6.d /var/log/ /var/tmp/ /run/nginx /var/run/
+chown -R "${UID}:${GID}" /kanboard /etc/php7 /etc/nginx /etc/s6.d /var/log/ /var/tmp/ /run/nginx /var/run/ /var/lib/nginx
 su-exec "$UID:$GID" /bin/s6-svscan /etc/s6.d
