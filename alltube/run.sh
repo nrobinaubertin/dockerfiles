a73199c715d6ed26284e5375a5d1392275f2427a
Niels Robin-Aubertin <git@niels.fr>
2020-02-18 15:44:23 +0100

    Create missing directory in alltube

--- alltube/run.sh
+++ alltube/run.sh
@@ -7,7 +7,7 @@ crontab -r
 # everyday at 1:22 AM
 echo "22 1 * * * update_ytdl.sh" | crontab -
 
-mkdir -p /run/nginx
+mkdir -p /run/nginx /var/tmp/nginx
 /usr/local/bin/update_ytdl.sh
 
 chown -R "alltube:alltube" /etc/s6.d /var/tmp/nginx /alltube /etc/php7 /etc/nginx /var/log /run /var/lib/nginx
