3bafcc785b2f0e14b562134fb1a6134997e65a27
Niels Robin-Aubertin <git@niels.fr>
2020-03-27 17:21:38 +0100

    Fix wordpress container

--- NOT_READY/wordpress/run.sh
+++ NOT_READY/wordpress/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 mv -n /wordpress/wp-content-original/* /wordpress/wp-content/
 
 sed -i -e "s|<AUTH_KEY>|$(openssl rand -base64 48)|g" \
@@ -11,12 +13,12 @@ sed -i -e "s|<AUTH_KEY>|$(openssl rand -base64 48)|g" \
        -e "s|<LOGGED_IN_SALT>|$(openssl rand -base64 48)|g" \
        -e "s|<NONCE_SALT>|$(openssl rand -base64 48)|g" /wordpress/wp-config.php
 
-mkdir -p /run/nginx
+mkdir -p /run/nginx /var/tmp/nginx /run/mysqld
 chown -R "${UID}:${GID}" /etc/s6.d /wordpress /etc/php7 /etc/nginx /var/lib/nginx /var/log /run /var/tmp/nginx
 su-exec "${UID}:${GID}" mysql_install_db --datadir="/wordpress/wp-content/mysql"
 (
     sleep 10
-    su-exec "${UID}:${GID}" mysqladmin -u root password "root"
-    su-exec "${UID}:${GID}" mysqladmin -u root --password="root" create "wordpress"
+    mysqladmin -u root password "root"
+    mysqladmin -u root --password="root" create "wordpress"
 ) &
 exec su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
