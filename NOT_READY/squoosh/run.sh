058e027e43f5b328132379c543491bf2a167c771
Niels Robin-Aubertin <git@niels.fr>
2020-09-06 07:45:43 +0200

    Update squoosh

--- NOT_READY/squoosh/run.sh
+++ NOT_READY/squoosh/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-chown -R "${UID}:${GID}" /squoosh /etc/s6.d /etc/nginx /var/log/ /var/tmp/ /run/nginx
+chown -R "${UID}:${GID}" /squoosh /etc/s6.d /etc/nginx /var/log/ /var/tmp/ /run/nginx /var/lib/nginx
 exec su-exec "${UID}:${GID}" /bin/s6-svscan /etc/s6.d
